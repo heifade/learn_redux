610b756ec031cfa2a09418efd0595bcbb6eef4ab
heifade <heifade@126.com>
2018-05-26 19:25:12 +0800

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -1,58 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { UserList, UserData } from "./userList";
-import { createStore, Action, combineReducers } from "redux";
+import { UserListC } from "./userList";
+
 
 let div = document.createElement("div");
 document.body.appendChild(div);
 
-let userList = new Array<UserData>();
-userList.push({ id: "1", name: "1" });
-userList.push({ id: "1", name: "1" });
-userList.push({ id: "1", name: "1" });
-userList.push({ id: "1", name: "1" });
-userList.push({ id: "1", name: "1" });
+
 
 ReactDOM.render(
   <div>
-    <UserList userList={userList} />
+    <UserListC />
   </div>,
   div
 );
 
-class MainState {
-  userList: Array<UserData>;
 
-  constructor() {
-    this.userList = [];
-  }
-}
-
-// function reducer(state: MainState = new MainState(), action: Action) {
-//   return {
-//     userList: reducerUserList(state.userList, action)
-//   };
-// }
-
-function reducerUserList(state: Array<UserData> = [], action: Action) {
-  switch (action.type) {
-    case "add":
-      return state.concat({
-        id: "2",
-        name: "2"
-      });
-    case "dele":
-      return state.shift();
-    default:
-      return state;
-  }
-}
-
-let reducer = combineReducers({ userList: reducerUserList });
-
-let store = createStore(reducer);
-store.subscribe(() => {
-  console.log(1, store.getState());
-});
-
-store.dispatch({ type: 1 });
--- src/userList.tsx
+++ src/userList.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import { createStore, Action, combineReducers, Dispatch } from "redux";
+import { connect } from "react-redux";
 
 export interface UserData {
   id: string;
@@ -6,7 +8,7 @@ export interface UserData {
 }
 
 export interface Props {
-  userList: Array<UserData>;
+  userList?: Array<UserData>;
 }
 
 export class UserList extends React.Component<Props, any> {
@@ -14,27 +16,96 @@ export class UserList extends React.Component<Props, any> {
     super(props, context);
   }
 
-  edit = (user: UserData) => {
+  edit = (user: UserData) => {};
+  dele = (user: UserData) => {};
 
-  }
-  dele = (user: UserData) => {
-
-  }
+  fetch = () => {};
 
   render() {
     return (
-      <table>
-        <tbody>
-          {this.props.userList.map((user, index) => (
-            <tr key={index}>
-              <td>{user.id}</td>
-              <td>{user.name}</td>
-              <td onClick={() => this.edit(user)}>编辑</td>
-              <td onClick={() => this.dele(user)}>删除</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        <button type="button" onClick={this.fetch}>
+          查询
+        </button>
+        <table>
+          <tbody>
+            {(this.props.userList || []).map((user, index) => (
+              <tr key={index}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td onClick={() => this.edit(user)}>编辑</td>
+                <td onClick={() => this.dele(user)}>删除</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
+
+
+
+class MainState {
+  userList: Array<UserData>;
+
+  constructor() {
+    this.userList = [];
+  }
+}
+
+// function reducer(state: MainState = new MainState(), action: Action) {
+//   return {
+//     userList: reducerUserList(state.userList, action)
+//   };
+// }
+
+function reducerUserList(state: Array<UserData> = [], type: string) {
+  switch (type) {
+    case "fetch":
+      let userList = new Array<UserData>();
+      userList.push({ id: "1", name: "1" });
+      userList.push({ id: "1", name: "1" });
+      userList.push({ id: "1", name: "1" });
+      userList.push({ id: "1", name: "1" });
+      userList.push({ id: "1", name: "1" });
+      return userList;
+    case "add":
+      return state.concat({
+        id: "2",
+        name: "2"
+      });
+    case "dele":
+      return state.shift();
+    default:
+      return state;
+  }
+}
+
+let reducer = combineReducers({ userList: reducerUserList });
+
+let store = createStore(reducer);
+store.subscribe(() => {
+  console.log(1, store.getState());
+});
+
+store.dispatch({ type: 1 });
+
+const stateToProps = (state: MainState) => {
+  return {
+    userList: reducerUserList(state.userList, "fetch")
+  }
+}
+
+const dispatchToProps = (dispath: Dispatch) => {
+  return {
+    fetch: () => {
+      dispath({
+        type: "fetch"
+      });
+    }
+  };
+};
+
+
+export let UserListC = connect(stateToProps, dispatchToProps)(UserList);
